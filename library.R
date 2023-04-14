b470e076a50e537e5e54459bcac20ceed117f95e
OlenaShcherbakova <alyona96shcherbakova@gmail.com>
2023-04-14 19:11:15 +0200

    Update library.R

--- library.R
+++ library.R
@@ -1,11 +1,11 @@
-#Run this script first. The input data file is based Grambank v. 1.0 and Glottolog v.4.4. 
-
-#brms/rstan works only for older R versions and rtools: https://github.com/stan-dev/rstan/issues/1011, so all brms analyses were run on Ubuntu 18.04.5 cluster 
+#Run this script first. The input data file is based Grambank v. 1.0 and Glottolog v.4.4.
 
 #necessary only for running on cluster
 #.libPaths(c("rlib/", .libPaths()))
 
-if (!suppressPackageStartupMessages(require("pacman"))) { install.packages("pacman") }
+if (!suppressPackageStartupMessages(require("pacman"))) {
+  install.packages("pacman")
+}
 
 pacman::p_load(
   #Matrix, #loading this package to avoid problems with tidyverse download in R.4.2 as described here https://github.com/tidymodels/tune/issues/362
@@ -23,7 +23,7 @@ pacman::p_load(
   tidybayes,
   bayesplot,
   
-  #plotting 
+  #plotting
   viridis,
   ggtree,
   RColorBrewer,
@@ -47,17 +47,23 @@ pacman::p_load(
   ggnewscale
 )
 
-OUTPUTDIR_output<- here("output")		
-# create output dir if it does not exist.		
-if (!dir.exists(OUTPUTDIR_output)) { dir.create(OUTPUTDIR_output) }
+OUTPUTDIR_output <- here("output")
+# create output dir if it does not exist.
+if (!dir.exists(OUTPUTDIR_output)) {
+  dir.create(OUTPUTDIR_output)
+}
 
-OUTPUTDIR_output_tables<- here("output_tables")		
-# create output dir if it does not exist.		
-if (!dir.exists(OUTPUTDIR_output_tables)) { dir.create(OUTPUTDIR_output_tables) }
+OUTPUTDIR_output_tables <- here("output_tables")
+# create output dir if it does not exist.
+if (!dir.exists(OUTPUTDIR_output_tables)) {
+  dir.create(OUTPUTDIR_output_tables)
+}
 
-OUTPUTDIR_output_models<- here("output_models")		
-# create output dir if it does not exist.		
-if (!dir.exists(OUTPUTDIR_output_models)) { dir.create(OUTPUTDIR_output_models) }
+OUTPUTDIR_output_models <- here("output_models")
+# create output dir if it does not exist.
+if (!dir.exists(OUTPUTDIR_output_models)) {
+  dir.create(OUTPUTDIR_output_models)
+}
 
 
 # quiet down, tidyverse:
@@ -77,21 +83,39 @@ get_trait_vector <- function(tree, data, variable) {
   x
 }
 
-load_data_NAs <- function(filename="data/GB_input.tsv") {
-  
-  grambank <- read.csv(filename, header = TRUE, sep = '\t', stringsAsFactors=FALSE)
-  colnames(grambank)[colnames(grambank)=="Language_ID"] <- "Glottocode"
-  grambank <- grambank[!with(grambank, is.na(GB070) | is.na(GB131) | is.na(GB132) | is.na(GB133) | is.na(GB136)),]
-  grambank_phylopath_compl <- subset(x = grambank, select = c("Glottocode", "GB070", "GB131", "GB132", "GB133", "GB136"))
+if (!dir.exists("grambank-analysed")) {
+  source("get_external_data.R")
+}
+source("obtaining_files_from_grambank-analysed.R")
+source("generating_GB_input_file.R")
+
+load_data_NAs <- function(filename = "data/GB_input.tsv") {
+  grambank <-
+    read.csv(
+      filename,
+      header = TRUE,
+      sep = '\t',
+      stringsAsFactors = FALSE
+    )
+  colnames(grambank)[colnames(grambank) == "Language_ID"] <-
+    "Glottocode"
+  grambank <-
+    grambank[!with(grambank,
+                   is.na(GB070) |
+                     is.na(GB131) | is.na(GB132) | is.na(GB133) | is.na(GB136)), ]
+  grambank_phylopath_compl <-
+    subset(
+      x = grambank,
+      select = c("Glottocode", "GB070", "GB131", "GB132", "GB133", "GB136")
+    )
   
   #preparing the file for analysis
   for (i in 1:nrow(grambank_phylopath_compl)) {
-    if ((grambank_phylopath_compl$GB131[i] == "0") & 
-        (grambank_phylopath_compl$GB132[i] == "0") & 
-        (grambank_phylopath_compl$GB133[i] == "1")
-    ) { 
+    if ((grambank_phylopath_compl$GB131[i] == "0") &
+        (grambank_phylopath_compl$GB132[i] == "0") &
+        (grambank_phylopath_compl$GB133[i] == "1")) {
       grambank_phylopath_compl$Verb_final[i] <- "1"
-    } 
+    }
     else {
       grambank_phylopath_compl$Verb_final[i] <- "0"
     }
@@ -100,7 +124,7 @@ load_data_NAs <- function(filename="data/GB_input.tsv") {
   for (i in 1:nrow(grambank_phylopath_compl)) {
     if (grambank_phylopath_compl$GB070[i] == "1") {
       grambank_phylopath_compl$Nominal_case[i] <- "1"
-    } 
+    }
     else {
       grambank_phylopath_compl$Nominal_case[i] <- "0"
     }
@@ -109,22 +133,34 @@ load_data_NAs <- function(filename="data/GB_input.tsv") {
   for (i in 1:nrow(grambank_phylopath_compl)) {
     if (grambank_phylopath_compl$GB136[i] == "0") {
       grambank_phylopath_compl$Free_word_order[i] <- "1"
-    } 
+    }
     else {
       grambank_phylopath_compl$Free_word_order[i] <- "0"
     }
   }
   
-  grambank_phylopath_compl <- subset(x = grambank_phylopath_compl, select = c("Glottocode", "Verb_final", "Free_word_order", "Nominal_case"))
+  grambank_phylopath_compl <-
+    subset(
+      x = grambank_phylopath_compl,
+      select = c(
+        "Glottocode",
+        "Verb_final",
+        "Free_word_order",
+        "Nominal_case"
+      )
+    )
   
   # make sure we have factors here.
   for (col in colnames(grambank_phylopath_compl)) {
-    grambank_phylopath_compl[[col]] <- as.factor(grambank_phylopath_compl[[col]])
+    grambank_phylopath_compl[[col]] <-
+      as.factor(grambank_phylopath_compl[[col]])
   }
-  rownames(grambank_phylopath_compl) <- grambank_phylopath_compl$Glottocode
+  rownames(grambank_phylopath_compl) <-
+    grambank_phylopath_compl$Glottocode
   grambank_phylopath_compl
 }
 
 source('varcovar.spatial_function.R')
 source('wrangling_tree.R')
+source('assigning_AUTOTYP_areas.R')
 source('generating_languages_sample_list.R')
