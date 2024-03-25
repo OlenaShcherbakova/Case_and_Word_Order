c1372d1e53adef4d9ad38b4aaae50d8f51d65bbb
OlenaShcherbakova <alyona96shcherbakova@gmail.com>
2024-03-25 09:52:03 +0100

    update model comparison related code and files

--- brms_models_comparison_tables_summary.R
+++ brms_models_comparison_tables_summary.R
@@ -1,19 +1,6 @@
 #combining tables from brms analysis outputs
 
 #NC model comparison
-loo <- read.csv("output_tables/loo_comparison.csv") %>%
-  rename(Model = "X") %>%
-  mutate(`Cross-validation type` = "LOO-CV") %>%
-  mutate_if(is.numeric, round, digits=2) %>%
-  relocate(`Cross-validation type`, .after=se_diff) %>%
-  dplyr::select(1:4)
-
-loo_mm <- read.csv("output_tables/loo_comparison_mm.csv") %>%
-  rename(Model = "X") %>%
-  mutate(`Cross-validation type` = "LOO with moment matching") %>%
-  mutate_if(is.numeric, round, digits=2) %>%
-  relocate(`Cross-validation type`, .after=se_diff) %>%
-  dplyr::select(1:4)
 
 kfold <- read.csv("output_tables/kfold_comparison.csv") %>%
   rename(Model = "X") %>%
@@ -22,24 +9,11 @@ kfold <- read.csv("output_tables/kfold_comparison.csv") %>%
   relocate(`Cross-validation type`, .after=se_diff) %>%
   dplyr::select(1:4)
 
-comparison_NC <- as.data.frame(rbind(loo, loo_mm, kfold)) %>% 
+comparison_NC <- as.data.frame(rbind(kfold)) %>% 
   mutate(`Response variable` = "Case") %>% 
   relocate(`Response variable`, .after = "Model")
 
 #Vf model comparison
-loo <- read.csv("output_tables/loo_comparison_Vf.csv") %>%
-  rename(Model = "X") %>%
-  mutate(`Cross-validation type` = "LOO-CV") %>%
-  mutate_if(is.numeric, round, digits=2) %>%
-  relocate(`Cross-validation type`, .after=se_diff) %>%
-  dplyr::select(1:4)
-
-loo_mm <- read.csv("output_tables/loo_comparison_mm_Vf.csv") %>%
-  rename(Model = "X") %>%
-  mutate(`Cross-validation type` = "LOO with moment matching") %>%
-  mutate_if(is.numeric, round, digits=2) %>%
-  relocate(`Cross-validation type`, .after=se_diff) %>%
-  dplyr::select(1:4)
 
 kfold <- read.csv("output_tables/kfold_comparison_Vf.csv") %>%
   rename(Model = "X") %>%
@@ -48,25 +22,11 @@ kfold <- read.csv("output_tables/kfold_comparison_Vf.csv") %>%
   relocate(`Cross-validation type`, .after=se_diff) %>%
   dplyr::select(1:4)
 
-comparison_Vf <- as.data.frame(rbind(loo, loo_mm, kfold)) %>% 
+comparison_Vf <- as.data.frame(rbind(kfold)) %>% 
   mutate(`Response variable` = "Verb-final word order") %>% 
   relocate(`Response variable`, .after = "Model")
 
 #FWO model comparison
-loo <- read.csv("output_tables/loo_comparison_FWO.csv") %>%
-  rename(Model = "X") %>%
-  mutate(`Cross-validation type` = "LOO-CV") %>%
-  mutate_if(is.numeric, round, digits=2) %>%
-  relocate(`Cross-validation type`, .after=se_diff) %>%
-  dplyr::select(1:4)
-
-loo_mm <- read.csv("output_tables/loo_comparison_mm_FWO.csv") %>%
-  rename(Model = "X") %>%
-  mutate(`Cross-validation type` = "LOO with moment matching") %>%
-  mutate_if(is.numeric, round, digits=2) %>%
-  relocate(`Cross-validation type`, .after=se_diff) %>%
-  dplyr::select(1:4)
-
 kfold <- read.csv("output_tables/kfold_comparison_FWO.csv") %>%
   rename(Model = "X") %>%
   mutate(`Cross-validation type` = "K-fold CV") %>%
@@ -74,18 +34,19 @@ kfold <- read.csv("output_tables/kfold_comparison_FWO.csv") %>%
   relocate(`Cross-validation type`, .after=se_diff) %>%
   dplyr::select(1:4)
 
-comparison_FWO <- as.data.frame(rbind(loo, loo_mm, kfold)) %>% 
+comparison_FWO <- as.data.frame(rbind(kfold)) %>% 
   mutate(`Response variable` = "Flexible word order") %>% 
   relocate(`Response variable`, .after = "Model")
 
 comparison <- as.data.frame(rbind(comparison_NC, comparison_Vf, comparison_FWO))
 
 comparison <- comparison %>%
-  mutate(Model = 
-           recode(Model,
+  dplyr::mutate(Model = 
+           dplyr::recode(Model,
                 "model1" = "phylogenetic",
                 "model2" = "spatial",
-                "model3" = "spatiophylogenetic"))
+                "model3" = "spatiophylogenetic")) %>% 
+  dplyr::select(-5)
 
 write.csv(comparison, "output_tables/brms_models_comparison.csv")
 
@@ -95,7 +56,7 @@ comparison_doc <- comparison %>%
   autofit() %>%
   hline(part="all") %>%
   vline(part="all") %>%
-  merge_v(j=c("Cross-validation type", "Response variable")) %>%
+  merge_v(j=c("Response variable")) %>%
   fix_border_issues()
 
 save_as_docx(
